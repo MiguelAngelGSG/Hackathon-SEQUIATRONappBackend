b48752f574917f9963ce753b99f602b4a4168d98
Norbert-Harrow <corvusfh@gmail.com>
2024-07-04 09:52:29 +0200

    Implemented Delete (CRUD) function

--- src/main/java/com/droughtAppBackend/controllers/droughtAppBackendControllers.java
+++ src/main/java/com/droughtAppBackend/controllers/droughtAppBackendControllers.java
@@ -1,4 +1,31 @@
 package com.droughtAppBackend.controllers;
 
-public class droughtAppBackendControllers {
+import com.droughtappbackend.models.DroughtData;
+import com.droughtappbackend.services.DroughtDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/drought-data")
+public class DroughtDataController {
+
+    @Autowired
+    private DroughtDataService service;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteData(@PathVariable Long id) {
+        try {
+            if (service.deleteData(id)) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
